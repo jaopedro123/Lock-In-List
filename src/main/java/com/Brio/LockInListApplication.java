3f96ded2eb3669d3e05423c78179c870d559c0eb
joao <joao.ferreira01br@gmail.com>
2025-07-14 02:41:09 -0300

    ...

---
+++ src/main/java/com/Brio/LockInListApplication.java
@@ -0,0 +1,13 @@
+package com.Brio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockInListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockInListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Brio/LockInListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Brio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockInListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
